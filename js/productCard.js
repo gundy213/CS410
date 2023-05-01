9f8267bee3a230a5823d5be9db6f1932060fddf8
Mason Agee <masamas132@gmail.com>
2023-04-30 22:36:45 -0500

    removes bad comments, adds better comments

--- js/productCard.js
+++ js/productCard.js
@@ -1,22 +1,27 @@
-//DO NOT USE IN CURRENT STATE, WILL FIX LATER
+//stores html code
+var fullCode = "";
 
-const listDiv = document.querySelector('ul');
+//retireves local JSON file
 fetch("../json/products.json")
+    //reads json file
     .then(res => res.json())
-    .then(data => { console.log(data) })
     .then(data => {
-        data.forEach(post => {
-            var productCard = `<div class="col-md-4"><div class="card">` +
-                `<img src="${post.image[0]}"alt="${post.name}"class="card-img-top">` +
-                `<div class="card-body">` +
-                `<h5 class="card-title">${post.name}</h5>` +
-                `<p class="card-text">${post.description}</p>` +
-                `<p class="card-text">Price: $${post.price}</p>` +
-                `<button class="btn btn-primary add-to-cart" data-product-id="${post.id}">Add to Cart</button>` +
-                `</div></div></div>`;
+        //loops though entire array of products
+        for (var i = 0; i < data.product.length; i++) {
+            var productCard = `<div class="col-md-4">\n<div class="card">\n` +
+                `<img src="${data.product[i].image[0]}"alt="${data.product[i].name}"class="card-img-top">\n` +
+                `<div class="card-body">\n` +
+                `<h5 class="card-title">${data.product[i].name}</h5>\n` +
+                `<p class="card-text">${data.product[i].description}</p>\n` +
+                `<p class="card-text">Price: $${data.product[i].price}</p>\n` +
+                `<button class="btn btn-primary add-to-cart" data-product-id="${data.product[i].id}">Add to Cart</button>\n` +
+                `</div>\n</div>\n</div>\n`;
+            //logs for testing, can remove if wanted later
             console.log(productCard);
-            listDiv.insertAdjacentHTML('beforeend', productCard);
-            //listDiv.insertAdjacentHTML('beforeend', `<div>${product.name}</div>`);
-        })
+            //add single card to the  fullCode varaiable 
+            fullCode = fullCode + productCard;
+        }
+        //writes to "productCards" id html selector 
+        document.getElementById("productCards").innerHTML = fullCode;
     }
     );
