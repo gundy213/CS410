e296aeea9e6daf04871182bcf8e98bee38fdf9bd
Mason Agee <masamas132@gmail.com>
2023-05-01 19:48:42 -0500

    updtaed image tag to work properly

--- js/productCard.js
+++ js/productCard.js
@@ -7,14 +7,14 @@ fetch("../json/products.json")
     .then(res => res.json())
     .then(data => {
         //loops though entire array of products
-        for (var i = 0; i < data.product.length; i++) {
+        for (var i = 0; i < data.products.length; i++) {
             var productCard = `<div class="col-md-4">\n<div class="card">\n` +
-                `<img src="${data.product[i].image[0]}"alt="${data.product[i].name}"class="card-img-top">\n` +
+                `<img src="${data.products[i].image}"alt="${data.products[i].name}"class="card-img-top">\n` +
                 `<div class="card-body">\n` +
-                `<h5 class="card-title">${data.product[i].name}</h5>\n` +
-                `<p class="card-text">${data.product[i].description}</p>\n` +
-                `<p class="card-text">Price: $${data.product[i].price}</p>\n` +
-                `<button class="btn btn-primary add-to-cart" data-product-id="${data.product[i].id}">Add to Cart</button>\n` +
+                `<h5 class="card-title">${data.products[i].name}</h5>\n` +
+                `<p class="card-text">${data.products[i].description}</p>\n` +
+                `<p class="card-text">Price: $${data.products[i].price}</p>\n` +
+                `<button class="btn btn-primary add-to-cart" data-product-id="${data.products[i].id}">Add to Cart</button>\n` +
                 `</div>\n</div>\n</div>\n`;
             //logs for testing, can remove if wanted later
             console.log(productCard);
