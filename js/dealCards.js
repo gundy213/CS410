7fda8f7d3910c8fd687d2d0a030dca68b7d84d7a
gundy213 <gundy213@yahoo.com>
2023-05-02 17:57:05 -0500

    ritz carlton

--- js/dealCards.js
+++ js/dealCards.js
@@ -8,14 +8,16 @@ fetch("../json/deals.json")
     .then(data => {
         //loops though entire array of products
         for (var i = 0; i < data.deal.length; i++) {
-            var productCard = `<div class="col-md-4 py-3">\n<div class="card">\n` +
+            var productCard = `<div class="col-md-4 py-3">\n<div class="card h-100">\n` +
                 `<img src="${data.deal[i].image[0]}"alt="${data.deal[i].name}"class="card-img-top">\n` +
                 `<div class="card-body">\n` +
                 `<h5 class="card-title">${data.deal[i].name}</h5>\n` +
                 `<p class="card-text">${data.deal[i].description}</p>\n` +
                 `<p class="card-text">Old Price: <del>$${data.deal[i].oldprice}</del></p>\n` +
                 `<p class="card-text">Price: $${data.deal[i].price}</p>\n` +
-                `<button class="btn btn-primary add-to-cart" data-product-id="${data.deal[i].id}">Add to Cart</button>\n` +
+                `</div>\n` +
+                `<div class="card-footer d-flex align-items-end">` +
+                `<button class="btn btn-dark btn-small add-to-cart" data-product-id="${data.deal[i].id}">Add to Cart</button>\n` +
                 `</div>\n</div>\n</div>\n`;
             //logs for testing, can remove if wanted later
             console.log(productCard);
--- js/productCard.js
+++ js/productCard.js
@@ -8,13 +8,15 @@ fetch("../json/products.json")
     .then(data => {
         //loops though entire array of products
         for (var i = 0; i < data.products.length; i++) {
-            var productCard = `<div class="col-md-4 py-3">\n<div class="card">\n` +
+            var productCard = `<div class="col-md-4 py-3">\n<div class="card h-100">\n` +
                 `<img src="${data.products[i].image[0]}"alt="${data.products[i].name}"class="card-img-top">\n` +
                 `<div class="card-body">\n` +
                 `<h5 class="card-title">${data.products[i].name}</h5>\n` +
                 `<p class="card-text">${data.products[i].description}</p>\n` +
                 `<p class="card-text">Price: $${data.products[i].price}</p>\n` +
-                `<button class="btn btn-primary add-to-cart" data-product-id="${data.products[i].id}">Add to Cart</button>\n` +
+                `</div>\n` +
+                `<div class="card-footer d-flex align-items-end">` +
+                `<button class="btn btn-dark btn-small add-to-cart" data-product-id="${data.products[i].id}">Add to Cart</button>\n` +
                 `</div>\n</div>\n</div>\n`;
             //logs for testing, can remove if wanted later
             console.log(productCard);
