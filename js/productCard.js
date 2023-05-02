7bb9ffd964c35f2f1db1df3d403c4078904da6ce
Mason Agee <masamas132@gmail.com>
2023-05-01 19:56:20 -0500

    quick change to make images arrays again

--- js/productCard.js
+++ js/productCard.js
@@ -9,7 +9,7 @@ fetch("../json/products.json")
         //loops though entire array of products
         for (var i = 0; i < data.products.length; i++) {
             var productCard = `<div class="col-md-4">\n<div class="card">\n` +
-                `<img src="${data.products[i].image}"alt="${data.products[i].name}"class="card-img-top">\n` +
+                `<img src="${data.products[i].image[0]}"alt="${data.products[i].name}"class="card-img-top">\n` +
                 `<div class="card-body">\n` +
                 `<h5 class="card-title">${data.products[i].name}</h5>\n` +
                 `<p class="card-text">${data.products[i].description}</p>\n` +
