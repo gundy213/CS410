e01e74370c319aec6f2dd18ae3acabe8df13d160
Caveney <102193290+Caveney@users.noreply.github.com>
2023-05-02 15:12:24 -0500

    works but for some reason also adds the reg productCards to the dealCards

--- js/dealCards.js
+++ js/dealCards.js
@@ -2,27 +2,27 @@
 var fullCode = "";
 
 //retireves local JSON file
-fetch("../json/products.json")
+fetch("../json/deals.json")
     //reads json file
     .then(res => res.json())
     .then(data => {
         //loops though entire array of products
         for (var i = 0; i < data.product.length; i++) {
-            var productCard = `<div class="col-md-4">\n<div class="card">\n` +
+            var dealCard = `<div class="col-md-4">\n<div class="card">\n` +
                 `<img src="${data.product[i].image[0]}"alt="${data.product[i].name}"class="card-img-top">\n` +
                 `<div class="card-body">\n` +
                 `<h5 class="card-title">${data.product[i].name}</h5>\n` +
                 `<p class="card-text">${data.product[i].description}</p>\n` +
                 `<p class="card-text">${data.product[i].category}</p>\n` +
-                `<p class="card-text">Price: $${data.product[i].price}</p>\n` +
+                `<p class="card-text">Price: <del>$${data.product[i].price}</del>$${data.product[i].dealprice}</p>\n` +
                 `<button class="btn btn-primary add-to-cart" data-product-id="${data.product[i].id}">Add to Cart</button>\n` +
                 `</div>\n</div>\n</div>\n`;
             //logs for testing, can remove if wanted later
-            console.log(productCard);
+            console.log(dealCard);
             //add single card to the  fullCode varaiable 
-            fullCode = fullCode + productCard;
+            fullCode = fullCode + dealCard;
         }
-        //writes to "productCards" id html selector 
-        document.getElementById("productCards").innerHTML = fullCode;
+        //writes to "dealCards" id html selector 
+        document.getElementById("dealCards").innerHTML = fullCode;
     }
     );
