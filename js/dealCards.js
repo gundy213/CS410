36bd47c1931dfe3b83b803fd6afdb28503af3106
Caveney <102193290+Caveney@users.noreply.github.com>
2023-05-02 16:06:54 -0500

    idfk kill me :)

--- js/dealCards.js
+++ js/dealCards.js
@@ -7,19 +7,19 @@ fetch("../json/deals.json")
     .then(res => res.json())
     .then(data => {
         //loops though entire array of products
-        for (var i = 0; i < data.deal.length; i++) {
-            var dealCard = `<div class="col-md-4">\n<div class="card">\n` +
-                `<img src="${data.deal[i].image[0]}"alt="${data.deal[i].name}"class="card-img-top">\n` +
+        for (var i = 0; i < data.products.length; i++) {
+            var productCard = `<div class="col-md-4">\n<div class="card">\n` +
+                `<img src="${data.products[i].image[0]}"alt="${data.products[i].name}"class="card-img-top">\n` +
                 `<div class="card-body">\n` +
-                `<h5 class="card-title">${data.deal[i].name}</h5>\n` +
-                `<p class="card-text">${data.deal[i].description}</p>\n` +
-                `<p class="card-text">Price: <del>$${data.deal[i].price}</del>$${data.deal[i].dealprice}</p>\n` +
-                `<button class="btn btn-primary add-to-cart" data-product-id="${data.deal[i].id}">Add to Cart</button>\n` +
+                `<h5 class="card-title">${data.products[i].name}</h5>\n` +
+                `<p class="card-text">${data.products[i].description}</p>\n` +
+                `<p class="card-text">Old Price: <del>$${data.products[i].price}</del></p>\n` +
+                `<button class="btn btn-primary add-to-cart" data-product-id="${data.products[i].id}">Add to Cart</button>\n` +
                 `</div>\n</div>\n</div>\n`;
             //logs for testing, can remove if wanted later
             console.log(dealCard);
             //add single card to the  fullCode varaiable 
-            fullCode = fullCode + dealCard;
+            fullCode = fullCode + productCard;
         }
         //writes to "dealCards" id html selector 
         document.getElementById("dealCards").innerHTML = fullCode;
